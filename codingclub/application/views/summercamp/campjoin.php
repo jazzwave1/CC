b499e468e5bb9e42734130cb93e8c6570ff0ad78
LeeHoJun <leehojun@LeeHoJunui-MacBook-Pro.local>
2016-07-18 21:18:00 +0900

    썸머캠프페이지 링크 추가

--- codingclub/application/views/summercamp/campjoin.php
+++ codingclub/application/views/summercamp/campjoin.php
@@ -45,7 +45,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="./"><strong>주니어</strong> 썸머 캠프</a>
+          <a class="navbar-brand" href="http://codingclubs.org"><strong>주니어</strong> 썸머 캠프</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right" >
