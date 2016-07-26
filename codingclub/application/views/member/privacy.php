831b0b7118ca79faa2251f9e3f17fc44f40ec294
LeeHoJun <leehojun@LeeHoJunui-MacBook-Pro.local>
2016-07-26 14:24:09 +0900

    링크 버그 수정

--- codingclub/application/views/member/privacy.php
+++ codingclub/application/views/member/privacy.php
@@ -63,7 +63,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="./">주니어 썸머 캠프</a>
+          <a class="navbar-brand" href="http://codingclubs.org">주니어 썸머 캠프</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right" >
