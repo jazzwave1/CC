b94e4c70a257e58b85478588001439197824d44a
LeeHoJun <leehojun@LeeHoJunui-MacBook-Pro.local>
2016-09-09 20:44:12 +0900

    logoutlink modify

--- codingclub/application/views/club/juniorsoftware.php
+++ codingclub/application/views/club/juniorsoftware.php
@@ -62,7 +62,7 @@
             <li class="sign_in" style="display:none"><a href="signup.html">회원가입</a></li>
             <li class="my_page"><a href="mypage.html">마이페이지</a></li>
             <li class="sign_up" style="display:none"><a href="login.html">로그인</a></li>
-            <li class="sign_out"><a href="main.html">로그아웃</a></li>
+            <li class="sign_out" id="bLogout"><a href="main.html">로그아웃</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -179,6 +179,14 @@
     <!-- contents :: e -->
   </section>
   <!-- container :: e -->
+   <script>   
+      $(function(){
+        $('#bLogout').click(function(){
+          window.location.replace("<?=HOSTURL?>/Login/Logout"); 
+        });
+      });
+   </script>   
+
 
 </body>
 
